875804a7552f521f998083fffacc0d381764378b
Musa Anday Arzu <musanday@gmail.com>
2019-05-22 19:08:53 +0200

    WebPack config changed

--- webpack.config.js
+++ webpack.config.js
@@ -20,11 +20,7 @@ module.exports = {
             {
                 test: /\.(png|svg|jpg|gif)$/,
                 use: [{
-                    loader : 'file-loader',
-                    options: {
-                        publicPath: '../dist',
-                        useRelativePaths: true
-                    }
+                    loader : 'file-loader'
                 }]
             },
             {
